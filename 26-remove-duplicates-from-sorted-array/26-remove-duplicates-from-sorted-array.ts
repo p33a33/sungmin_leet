8bc923bb0241148d1f09a7438cbad3eb06f65b03
Sungmin Cho <66895623+p33a33@users.noreply.github.com>
2022-08-26 20:46:41 +0900

    Time: 120 ms (63.68%), Space: 45.7 MB (35.78%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.ts
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.ts
@@ -1,9 +1,7 @@
 function removeDuplicates(nums: number[]): number {
     let i = 0;
     for(let j = 1; j < nums.length; j++) {
-        if(nums[i] === nums[j]) {
-            continue;
-        } else {
+        if(nums[i] !== nums[j]) {
             i++;
             nums[i] = nums[j];
         }
