502c122140ee95eeae2320c8fa3f0fb735751c3a
Sungmin Cho <66895623+p33a33@users.noreply.github.com>
2022-09-01 22:14:16 +0900

    Time: 100 ms (50.99%), Space: 44.7 MB (27.40%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.ts
+++ 35-search-insert-position/35-search-insert-position.ts
@@ -1,32 +1,31 @@
 function searchInsert(nums: number[], target: number): number {
-    let result = 0;
+    let start = 0;
+    let end = nums.length - 1
     
-    while(nums.length > 0) {
-    const mid = Math.floor((nums.length - 1) / 2);
+    while(start <= end) {
+        const mid = Math.floor((start + end)/2);
         
-    if(nums.length === 1 && target !== nums[mid]) {
-        return nums[mid] > target ? result : result + 1
-    }
-        
-    if(target === nums[mid]) {
-        return result + mid
-    }    
-            
-    if(target > nums[mid]) { 
-        result += mid+1
-        nums = nums.slice(mid + 1)
-    }
-        
-    if(target < nums[mid]) {
-        nums = nums.slice(0, mid + 1)
-    }
+        if(target === nums[mid]) {
+            return mid
+        }    
+
+        if(start === end) {
+           return target > nums[start] ? start+1 : start
+        }
+
+        if(target > nums[mid]) { 
+            start = mid+1
+        }
+
+        if(target < nums[mid]) {
+            end = mid-1
+        }
     }
 
-    
-    
-    return result
+    return start;
 };
-
-//mid
-// 1      3             5      6
-//   HEAD                 TAIL
+// str                 
+// end
+// mid
+//  1      3             4      6
+//    HEAD                 TAIL
